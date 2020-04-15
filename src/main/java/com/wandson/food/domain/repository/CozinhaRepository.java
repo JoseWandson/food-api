f08c2268863c65e2905a9347c5786edcf7961158
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-04-14 23:23:17 -0300

    Conhecendo e implementando o padrão Repository

---
+++ src/main/java/com/wandson/food/domain/repository/CozinhaRepository.java
@@ -0,0 +1,17 @@
+package com.wandson.food.domain.repository;
+
+import java.util.List;
+
+import com.wandson.food.domain.model.Cozinha;
+
+public interface CozinhaRepository {
+
+	List<Cozinha> listar();
+
+	Cozinha buscar(Long id);
+
+	Cozinha salvar(Cozinha cozinha);
+
+	void remover(Cozinha cozinha);
+
+}
