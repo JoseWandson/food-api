0c40f07a9d0a6e51b3b2c647445b0f6d65cf8c38
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-05-19 21:45:18 -0300

    Adicionando repositório de restaurantes; Atualizando dependências.

---
+++ src/main/java/com/wandson/food/domain/repository/RestauranteRepository.java
@@ -0,0 +1,17 @@
+package com.wandson.food.domain.repository;
+
+import java.util.List;
+
+import com.wandson.food.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+
+	List<Restaurante> listar();
+
+	Restaurante buscar(Long id);
+
+	Restaurante salvar(Restaurante restaurante);
+
+	void remover(Restaurante restaurante);
+
+}
