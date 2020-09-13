59b8cc0d8199ebe209ad3e641f980552c3634a3b
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-09-13 17:45:40 -0300

    Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/wandson/food/api/controller/CozinhaController.java
+++ src/main/java/com/wandson/food/api/controller/CozinhaController.java
@@ -1,9 +1,11 @@
 package com.wandson.food.api.controller;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,8 +33,12 @@ public class CozinhaController {
 	}
 
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		if (Objects.nonNull(cozinha)) {
+			return ResponseEntity.ok(cozinha);
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
