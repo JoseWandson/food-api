cbc6278627d3737e8007bf0c6a46e1629faea5af
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-09-29 23:41:58 -0300

    Implementando a inclusão de recursos de restaurantes

--- src/main/java/com/wandson/food/api/controller/RestauranteController.java
+++ src/main/java/com/wandson/food/api/controller/RestauranteController.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wandson.food.domain.exception.EntidadeNaoEncontradaException;
 import com.wandson.food.domain.model.Restaurante;
 import com.wandson.food.domain.service.RestauranteService;
 
@@ -32,6 +37,17 @@ public class RestauranteController {
 			return ResponseEntity.ok(restaurante);
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			restaurante = restauranteService.salvar(restaurante);
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 
 }
--- src/main/java/com/wandson/food/domain/service/RestauranteService.java
+++ src/main/java/com/wandson/food/domain/service/RestauranteService.java
@@ -1,15 +1,21 @@
 package com.wandson.food.domain.service;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wandson.food.domain.exception.EntidadeNaoEncontradaException;
+import com.wandson.food.domain.model.Cozinha;
 import com.wandson.food.domain.model.Restaurante;
 import com.wandson.food.domain.repository.RestauranteRepository;
 
 @Service
 public class RestauranteService {
+
+	@Autowired
+	private CozinhaService cozinhaService;
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
@@ -20,6 +26,18 @@ public class RestauranteService {
 
 	public Restaurante buscar(Long restauranteId) {
 		return restauranteRepository.buscar(restauranteId);
+	}
+
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaService.buscar(cozinhaId);
+
+		if (Objects.isNull(cozinha)) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		restaurante.setCozinha(cozinha);
+		return restauranteRepository.salvar(restaurante);
 	}
 
 }
