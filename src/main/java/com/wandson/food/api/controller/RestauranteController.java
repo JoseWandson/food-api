f74cbfba462ba246cd3b5c585de7958be3d8520c
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-12-13 21:41:39 -0300

    Adicionando as datas de cadastro e atualização do restaurante

--- src/main/java/com/wandson/food/api/controller/RestauranteController.java
+++ src/main/java/com/wandson/food/api/controller/RestauranteController.java
@@ -61,7 +61,8 @@ public class RestauranteController {
 			Optional<Restaurante> restauranteAtual = restauranteService.buscar(restauranteId);
 
 			if (restauranteAtual.isPresent()) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco");
+				BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco",
+						"dataCadastro");
 
 				Restaurante restauranteSalvo = restauranteService.salvar(restauranteAtual.get());
 				return ResponseEntity.ok(restauranteSalvo);
