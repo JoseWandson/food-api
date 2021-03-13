5b6d822fbaec0a1ad5a12412f41014f304a54134
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-03-13 14:59:15 -0300

    Tratando exceções em nível de controlador com @ExceptionHandler

--- src/main/java/com/wandson/food/api/controller/CidadeController.java
+++ src/main/java/com/wandson/food/api/controller/CidadeController.java
@@ -1,11 +1,14 @@
 package com.wandson.food.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wandson.food.api.exceptionhandler.Problema;
+import com.wandson.food.domain.exception.EntidadeNaoEncontradaException;
 import com.wandson.food.domain.exception.EstadoNaoEncontradoException;
 import com.wandson.food.domain.exception.NegocioException;
 import com.wandson.food.domain.model.Cidade;
@@ -62,6 +67,18 @@ public class CidadeController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cidadeId) {
 		cidadeService.excluir(cidadeId);
+	}
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<Problema> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	}
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Problema> tratarNegocioException(NegocioException e) {
+		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
 	}
 
 }
