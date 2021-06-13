7d7790c50f64faab03afc4160ca0a8e2faebcd61
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-06-13 15:42:51 -0300

    Customizando e resolvendo mensagens de validação globais em Resource Bundle

--- src/main/java/com/wandson/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/wandson/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +39,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private static final String MSG_ERRO_GENERICA_USUARIO_FINAL = "Ocorreu um erro interno inesperado no sistema. "
 			+ "Tente novamente e se o problema persistir, entre em contato com o administrador do sistema.";
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<Object> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
@@ -135,9 +141,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		var problemType = ProblemType.DADOS_INVALIDOS;
 		var detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
 		var bindingResult = ex.getBindingResult();
-		List<Field> problemFields = bindingResult.getFieldErrors().stream().map(fieldError -> Field.builder()
-				.name(fieldError.getField()).userMessage(fieldError.getDefaultMessage()).build())
-				.collect(Collectors.toList());
+
+		List<Field> problemFields = bindingResult.getFieldErrors().stream().map(fieldError -> {
+			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			return Field.builder().name(fieldError.getField()).userMessage(message).build();
+		}).collect(Collectors.toList());
+
 		var problem = createProblemBuilder(status, problemType, detail).userMessage(detail).fields(problemFields)
 				.build();
 		return handleExceptionInternal(ex, problem, headers, status, request);
