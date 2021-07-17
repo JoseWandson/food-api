24799a1be1e968c969e3cf618b04205d38ef306d
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-07-17 18:52:27 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/wandson/food/CadastroCozinhaIT.java
+++ src/test/java/com/wandson/food/CadastroCozinhaIT.java
@@ -1,13 +1,15 @@
 package com.wandson.food;
 
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,10 +20,18 @@ class CadastroCozinhaIT {
 
 	@Test
 	void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		enableLoggingOfRequestAndResponseIfValidationFails();
 
 		given().basePath("/cozinhas").port(port).accept(ContentType.JSON).when().get().then()
 				.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		enableLoggingOfRequestAndResponseIfValidationFails();
+
+		given().basePath("/cozinhas").port(port).accept(ContentType.JSON).when().get().then().body("", hasSize(4))
+				.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 
 }
