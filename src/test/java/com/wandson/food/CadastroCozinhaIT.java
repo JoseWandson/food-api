892d4c25a0ac2be0723f60c7ac2fb6f0bc2aefc1
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-07-17 19:13:17 -0300

    Criando um método para fazer setup dos testes

--- src/test/java/com/wandson/food/CadastroCozinhaIT.java
+++ src/test/java/com/wandson/food/CadastroCozinhaIT.java
@@ -1,15 +1,16 @@
 package com.wandson.food;
 
-import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,20 +19,22 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@BeforeEach
+	void setup() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+	}
+
 	@Test
 	void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		enableLoggingOfRequestAndResponseIfValidationFails();
-
-		given().basePath("/cozinhas").port(port).accept(ContentType.JSON).when().get().then()
-				.statusCode(HttpStatus.OK.value());
+		given().accept(ContentType.JSON).when().get().then().statusCode(HttpStatus.OK.value());
 	}
 
 	@Test
 	void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-		enableLoggingOfRequestAndResponseIfValidationFails();
-
-		given().basePath("/cozinhas").port(port).accept(ContentType.JSON).when().get().then().body("", hasSize(4))
-				.body("nome", hasItems("Indiana", "Tailandesa"));
+		given().accept(ContentType.JSON).when().get().then().body("", hasSize(4)).body("nome",
+				hasItems("Indiana", "Tailandesa"));
 	}
 
 }
