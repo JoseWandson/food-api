a6350db716a6dcd82b052193f1b74fa28e933673
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-07-17 21:13:28 -0300

    Resolvendo o problema da ordem de execução dos testes voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/wandson/food/CadastroCozinhaIT.java
+++ src/test/java/com/wandson/food/CadastroCozinhaIT.java
@@ -4,8 +4,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -19,11 +21,16 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@Autowired
+	private Flyway flyway;
+
 	@BeforeEach
 	void setup() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+
+		flyway.migrate();
 	}
 
 	@Test
@@ -35,6 +42,12 @@ class CadastroCozinhaIT {
 	void deveConter4Cozinhas_QuandoConsultarCozinhas() {
 		given().accept(ContentType.JSON).when().get().then().body("", hasSize(4)).body("nome",
 				hasItems("Indiana", "Tailandesa"));
+	}
+
+	@Test
+	void testRetornarStatus201_QuandoCadastrarCozinha() {
+		given().body("{ \"nome\": \"Chinesa\" }").contentType(ContentType.JSON).accept(ContentType.JSON).when().post()
+				.then().statusCode(HttpStatus.CREATED.value());
 	}
 
 }
