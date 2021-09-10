e8561adf95dd5bcb05d7b7eb15e27e00c3cfff7d
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-09-10 14:42:21 -0300

    Implementando os endpoints de formas de pagamento

---
+++ src/main/java/com/wandson/food/api/assembler/FormaPagamentoModelAssembler.java
@@ -0,0 +1,27 @@
+package com.wandson.food.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.wandson.food.api.model.FormaPagamentoModel;
+import com.wandson.food.domain.model.FormaPagamento;
+
+@Component
+public class FormaPagamentoModelAssembler {
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public FormaPagamentoModel toModel(FormaPagamento formaPagamento) {
+		return modelMapper.map(formaPagamento, FormaPagamentoModel.class);
+	}
+
+	public List<FormaPagamentoModel> toCollectionModel(List<FormaPagamento> formasPagamentos) {
+		return formasPagamentos.stream().map(this::toModel).collect(Collectors.toList());
+	}
+
+}
