edb724bfa895aa107b4631ef5d7375a9e5cc8b1c
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-09-25 22:45:36 -0300

    Implementando os endpoints de produtos

---
+++ src/main/java/com/wandson/food/api/assembler/ProdutoModelAssembler.java
@@ -0,0 +1,27 @@
+package com.wandson.food.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.wandson.food.api.model.ProdutoModel;
+import com.wandson.food.domain.model.Produto;
+
+@Component
+public class ProdutoModelAssembler {
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public ProdutoModel toModel(Produto produto) {
+		return modelMapper.map(produto, ProdutoModel.class);
+	}
+
+	public List<ProdutoModel> toCollectionModel(List<Produto> produtos) {
+		return produtos.stream().map(this::toModel).collect(Collectors.toList());
+	}
+
+}
---
+++ src/main/java/com/wandson/food/domain/service/CadastroProdutoService.java
@@ -0,0 +1,27 @@
+package com.wandson.food.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wandson.food.domain.exception.ProdutoNaoEncontradoException;
+import com.wandson.food.domain.model.Produto;
+import com.wandson.food.domain.repository.ProdutoRepository;
+
+@Service
+public class CadastroProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Transactional
+	public Produto salvar(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+		return produtoRepository.findById(restauranteId, produtoId)
+				.orElseThrow(() -> new ProdutoNaoEncontradoException(restauranteId, produtoId));
+	}
+
+}
