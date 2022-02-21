8c03ad6f99efb31ec5d9c030059decbf417b4a68
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-02-21 20:56:59 -0300

    Validando o content type do arquivo

---
+++ src/main/java/com/wandson/food/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,26 @@
+package com.wandson.food.core.validation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+	private List<String> allowedContentTypes;
+
+	@Override
+	public void initialize(FileContentType constraint) {
+		allowedContentTypes = Arrays.asList(constraint.allowed());
+	}
+
+	@Override
+	public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext context) {
+		return Objects.isNull(multipartFile) || allowedContentTypes.contains(multipartFile.getContentType());
+	}
+
+}
