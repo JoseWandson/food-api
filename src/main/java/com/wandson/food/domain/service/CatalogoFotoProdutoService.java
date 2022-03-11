463fdf6259b757a55fdd63ea67f6fe58dab57ccb
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-03-11 11:54:21 -0300

    Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- src/main/java/com/wandson/food/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/wandson/food/domain/service/CatalogoFotoProdutoService.java
@@ -1,5 +1,6 @@
 package com.wandson.food.domain.service;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,24 +9,36 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.wandson.food.domain.model.FotoProduto;
 import com.wandson.food.domain.repository.ProdutoRepository;
+import com.wandson.food.domain.service.FotoStorageService.NovaFoto;
 
 @Service
 public class CatalogoFotoProdutoService {
 
 	@Autowired
+	private FotoStorageService fotoStorage;
+
+	@Autowired
 	private ProdutoRepository produtoRepository;
 
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
 		Long restauranteId = foto.getRestauranteId();
 		Long produtoId = foto.getProduto().getId();
+		String nomeNovoArquivo = fotoStorage.gerarNomeArquivo(foto.getNomeArquivo());
 
 		Optional<FotoProduto> fotoExistente = produtoRepository.findFotoById(restauranteId, produtoId);
 		if (fotoExistente.isPresent()) {
 			produtoRepository.delete(fotoExistente.get());
 		}
 
-		return produtoRepository.save(foto);
+		foto.setNomeArquivo(nomeNovoArquivo);
+		foto = produtoRepository.save(foto);
+		produtoRepository.flush();
+
+		NovaFoto novaFoto = NovaFoto.builder().nomeArquivo(nomeNovoArquivo).inputStream(dadosArquivo).build();
+		fotoStorage.armazenar(novaFoto);
+
+		return foto;
 	}
 
 }
--- src/main/java/com/wandson/food/domain/service/FotoStorageService.java
+++ src/main/java/com/wandson/food/domain/service/FotoStorageService.java
@@ -1,11 +1,16 @@
 package com.wandson.food.domain.service;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 import lombok.Builder;
 import lombok.Getter;
 
 public interface FotoStorageService {
+
+	default String gerarNomeArquivo(String nomeOriginal) {
+		return UUID.randomUUID().toString() + "_" + nomeOriginal;
+	}
 
 	void armazenar(NovaFoto novaFoto);
 
