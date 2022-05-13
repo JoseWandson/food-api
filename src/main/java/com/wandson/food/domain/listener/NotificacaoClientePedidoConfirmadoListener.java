727fb8a956083e8ab8465a299bdf3f78733a6404
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-05-13 11:53:33 -0300

    Reagindo a Domain Events após o commit da transação

--- src/main/java/com/wandson/food/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/wandson/food/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,8 +1,8 @@
 package com.wandson.food.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.wandson.food.domain.event.PedidoConfirmadoEvent;
 import com.wandson.food.domain.model.Pedido;
@@ -15,7 +15,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 
-	@EventListener
+	@TransactionalEventListener
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		Pedido pedido = event.getPedido();
 
