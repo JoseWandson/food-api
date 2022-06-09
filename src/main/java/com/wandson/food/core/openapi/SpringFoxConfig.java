674f01cca8e5cad00958a3fe0bb5546b94f3bf53
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-09 11:00:33 -0300

    Descrevendo informações da API na documentação

--- src/main/java/com/wandson/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/wandson/food/core/openapi/SpringFoxConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,7 +18,14 @@ public class SpringFoxConfig {
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.OAS_30).select()
-				.apis(RequestHandlerSelectors.basePackage("com.wandson.food.api")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.wandson.food.api")).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("WandFood Api").description("API aberta para clientes e restaurantes")
+				.version("1").contact(new Contact("Wandson",
+						"https://www.linkedin.com/in/wandson-cavalcante-b8428b139/", "wandsonacop@hotmail.com"))
+				.build();
 	}
 
 }
