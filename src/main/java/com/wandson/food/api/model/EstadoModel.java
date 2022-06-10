e4d7237dfa7ac970c5e13f1fbade6d79ad131b26
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-10 14:20:01 -0300

    Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/wandson/food/api/model/EstadoModel.java
+++ src/main/java/com/wandson/food/api/model/EstadoModel.java
@@ -1,5 +1,6 @@
 package com.wandson.food.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,9 @@ import lombok.Setter;
 @Setter
 public class EstadoModel {
 
+	@ApiModelProperty(example = "1")
 	private Long id;
+
+	@ApiModelProperty(example = "Paraíba")
 	private String nome;
 }
