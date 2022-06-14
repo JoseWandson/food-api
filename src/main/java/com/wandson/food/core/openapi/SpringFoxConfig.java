6085f74a7a7a202164fbb37b7596fc00d21fea7f
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-13 21:06:08 -0300

    Referenciando modelo de representação de problema com códigos de status de erro

--- src/main/java/com/wandson/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/wandson/food/core/openapi/SpringFoxConfig.java
@@ -2,17 +2,20 @@ package com.wandson.food.core.openapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.fasterxml.classmate.TypeResolver;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.wandson.food.api.exceptionhandler.Problem;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -55,7 +58,8 @@ public class SpringFoxConfig {
 	private List<Response> globalGetResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-						.description("Recurso não possui representação que pode ser aceita pelo consumidor").build(),
+						.description("Recurso não possui representação que pode ser aceita pelo consumidor")
+						.representation(MediaType.APPLICATION_JSON).apply(getProblemaModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
 						.description("Erro interno do Servidor").build());
 	}
@@ -63,21 +67,31 @@ public class SpringFoxConfig {
 	private List<Response> globalPostPutResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-						.description("Requisição inválida (erro do cliente)").build(),
+						.description("Requisição inválida (erro do cliente)").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
 						.description("Recurso não possui representação que poderia ser aceita pelo consumidor").build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-						.description("Requisição recusada porque o corpo está em um formato não suportado").build(),
+						.description("Requisição recusada porque o corpo está em um formato não suportado")
+						.representation(MediaType.APPLICATION_JSON).apply(getProblemaModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno no servidor").build());
+						.description("Erro interno no servidor").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build());
 	}
 
 	private List<Response> globalDeleteResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-						.description("Requisição inválida (erro do cliente)").build(),
+						.description("Requisição inválida (erro do cliente)").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno no servidor").build());
+						.description("Erro interno no servidor").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build());
+	}
+
+	private Consumer<RepresentationBuilder> getProblemaModelReference() {
+		return r -> r.model(m -> m.name("Problema").referenceModel(ref -> ref.key(k -> k
+				.qualifiedModelName(q -> q.name("Problema").namespace("com.wandson.food.api.exceptionhandler")))));
 	}
 
 }
