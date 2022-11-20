860a99db21d86bdf723d9fac6e2c1646fa0033b6
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-11-20 10:24:04 -0300

    Refatorando construção e inclusão de links em representation model

--- src/main/java/com/wandson/food/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/wandson/food/api/assembler/PedidoModelAssembler.java
@@ -5,14 +5,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariable.VariableType;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.wandson.food.api.Links;
 import com.wandson.food.api.controller.CidadeController;
 import com.wandson.food.api.controller.FormaPagamentoController;
 import com.wandson.food.api.controller.PedidoController;
@@ -28,6 +24,9 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 	@Autowired
 	private ModelMapper modelMapper;
 
+	@Autowired
+	private Links links;
+
 	public PedidoModelAssembler() {
 		super(PedidoController.class, PedidoModel.class);
 	}
@@ -37,29 +36,22 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 		PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
 		modelMapper.map(pedido, pedidoModel);
 
-		TemplateVariables pageVariables = new TemplateVariables(
-				new TemplateVariable("page", VariableType.REQUEST_PARAM),
-				new TemplateVariable("size", VariableType.REQUEST_PARAM),
-				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+		pedidoModel.add(links.linkToPedidos());
 
-		TemplateVariables filtroVariables = new TemplateVariables(
-				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
-
-		String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
-		pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables.concat(filtroVariables)), "pedidos"));
 		pedidoModel.getRestaurante().add(
 				linkTo(methodOn(RestauranteController.class).buscar(pedido.getRestaurante().getId())).withSelfRel());
+
 		pedidoModel.getCliente()
 				.add(linkTo(methodOn(UsuarioController.class).buscar(pedido.getCliente().getId())).withSelfRel());
+
 		pedidoModel.getFormaPagamento()
 				.add(linkTo(methodOn(FormaPagamentoController.class).buscar(pedido.getFormaPagamento().getId(), null))
 						.withSelfRel());
+
 		pedidoModel.getEnderecoEntrega().getCidade()
 				.add(linkTo(methodOn(CidadeController.class).buscar(pedido.getEnderecoEntrega().getCidade().getId()))
 						.withSelfRel());
+
 		pedidoModel.getItens().forEach(i -> i.add(linkTo(
 				methodOn(RestauranteProdutoController.class).buscar(pedido.getRestaurante().getId(), i.getProdutoId()))
 						.withRel("produto")));
