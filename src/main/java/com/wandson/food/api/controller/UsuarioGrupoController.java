5f6fa953e021b45473be087d4d4c710c129027f0
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-02-19 22:40:43 -0300

    Adicionando links de associação de usuários com grupos

--- src/main/java/com/wandson/food/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/wandson/food/api/controller/UsuarioGrupoController.java
@@ -2,16 +2,16 @@ package com.wandson.food.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wandson.food.api.Links;
 import com.wandson.food.api.assembler.GrupoModelAssembler;
 import com.wandson.food.api.model.GrupoModel;
 import com.wandson.food.api.openapi.controller.UsuarioGrupoControllerOpenApi;
@@ -28,26 +28,37 @@ public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 	@Autowired
 	private GrupoModelAssembler grupoModelAssembler;
 
+	@Autowired
+	private Links links;
+
 	@Override
 	@GetMapping
 	public CollectionModel<GrupoModel> listar(@PathVariable Long usuarioId) {
 		Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
 
-		return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
+		CollectionModel<GrupoModel> grupos = grupoModelAssembler.toCollectionModel(usuario.getGrupos()).removeLinks()
+				.add(links.linkToUsuarioGrupoAssociacao(usuarioId, "associar"));
+
+		grupos.getContent().forEach(
+				grupo -> grupo.add(links.linkToUsuarioGrupoDesassociacao(usuarioId, grupo.getId(), "desassociar")));
+
+		return grupos;
 	}
 
 	@Override
 	@DeleteMapping("/{grupoId}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+	public ResponseEntity<Void> desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
 		cadastroUsuario.desassociarGrupo(usuarioId, grupoId);
+
+		return ResponseEntity.noContent().build();
 	}
 
 	@Override
 	@PutMapping("/{grupoId}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+	public ResponseEntity<Void> associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
 		cadastroUsuario.associarGrupo(usuarioId, grupoId);
+
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/wandson/food/api/openapi/controller/UsuarioGrupoControllerOpenApi.java
+++ src/main/java/com/wandson/food/api/openapi/controller/UsuarioGrupoControllerOpenApi.java
@@ -1,6 +1,7 @@
 package com.wandson.food.api.openapi.controller;
 
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 
 import com.wandson.food.api.exceptionhandler.Problem;
 import com.wandson.food.api.model.GrupoModel;
@@ -23,12 +24,12 @@ public interface UsuarioGrupoControllerOpenApi {
 	@Operation(summary = "Desassociação de grupo com usuário")
 	@ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso")
 	@ApiResponse(responseCode = "404", description = "Usuário ou grupo não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
-	void desassociar(@Parameter(description = "ID do usuário", example = "1") Long usuarioId,
+	ResponseEntity<Void> desassociar(@Parameter(description = "ID do usuário", example = "1") Long usuarioId,
 			@Parameter(description = "ID do grupo", example = "1") Long grupoId);
 
 	@Operation(summary = "Associação de grupo com usuário")
 	@ApiResponse(responseCode = "204", description = "Associação realizada com sucesso")
 	@ApiResponse(responseCode = "404", description = "Usuário ou grupo não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
-	void associar(@Parameter(description = "ID do usuário", example = "1") Long usuarioId,
+	ResponseEntity<Void> associar(@Parameter(description = "ID do usuário", example = "1") Long usuarioId,
 			@Parameter(description = "ID do grupo", example = "1") Long grupoId);
 }
