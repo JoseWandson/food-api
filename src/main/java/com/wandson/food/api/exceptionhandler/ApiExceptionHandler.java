bfe4486d06330153eb602fd4ca076d883113e514
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-07-01 23:30:16 -0300

    Configurando o appender do Loggly no Logback

--- src/main/java/com/wandson/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/wandson/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
@@ -150,12 +149,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
-	}
-
-	@Override
-	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatusCode status,
-			WebRequest request) {
-		return handleValidationInternal(ex, ex, headers, status, request);
 	}
 
 	@Override
