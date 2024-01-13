d7911f2de71a7dd2f84b42b4443887707730bbb8
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2024-01-12 21:47:05 -0300

    Configurando autenticação de usuários em memória

--- src/main/java/com/wandson/food/core/security/WebSecurityConfig.java
+++ src/main/java/com/wandson/food/core/security/WebSecurityConfig.java
@@ -1,15 +1,25 @@
 package com.wandson.food.core.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+		auth.inMemoryAuthentication().withUser("wandson").password(passwordEncoder.encode("123")).roles("ADMIN").and()
+				.withUser("joao").password(passwordEncoder.encode("123")).roles("ADMIN");
+	}
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,6 +27,11 @@ public class WebSecurityConfig {
 				.requestMatchers("/v1/cozinhas/**").permitAll().anyRequest().authenticated())
 				.csrf(csrf -> csrf.disable());
 		return http.build();
+	}
+
+	@Bean
+	static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
